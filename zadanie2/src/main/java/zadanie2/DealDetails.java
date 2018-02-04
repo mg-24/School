a7562ae359822bcc8a591866abc066d415ab3a1a
Matus Gaspar <mg-24@azet.sk>
2018-02-04 01:10:56 +0100

    VyhladavanieInformaciiZadanie2added

---
+++ zadanie2/src/main/java/zadanie2/DealDetails.java
@@ -0,0 +1,34 @@
+package zadanie2;
+
+public class DealDetails {
+	
+	public int deal_id;
+	public String title_deal;
+	public String title_desc;
+	public String title_city;
+	public String getTitle_city() {
+		return title_city;
+	}
+	public void setTitle_city(String title_city) {
+		this.title_city = title_city;
+	}
+	public int getDeal_id() {
+		return deal_id;
+	}
+	public void setDeal_id(int deal_id) {
+		this.deal_id = deal_id;
+	}
+	public String getTitle_deal() {
+		return title_deal;
+	}
+	public void setTitle_deal(String title_deal) {
+		this.title_deal = title_deal;
+	}
+	public String getTitle_desc() {
+		return title_desc;
+	}
+	public void setTitle_desc(String title_desc) {
+		this.title_desc = title_desc;
+	}
+
+}
